a122dba04b98306b74b1acc6ae19f1387d4a9923
Kashevar <ayvengo53@gmail.com>
2023-03-02 20:55:46 +0400

    .

--- src/main/java/net/proselyte/springbootdemo/controller/UserController.java
+++ src/main/java/net/proselyte/springbootdemo/controller/UserController.java
@@ -1,7 +1,7 @@
 package net.proselyte.springbootdemo.controller;
 
 import net.proselyte.springbootdemo.model.User;
-import net.proselyte.springbootdemo.service.UserService;
+import net.proselyte.springbootdemo.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,16 +14,16 @@ import java.util.List;
 @Controller
 public class UserController {
 
-    private final UserService userService;
+    private final UserServiceImpl userServiceImpl;
 
     @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
+    public UserController(UserServiceImpl userServiceImpl) {
+        this.userServiceImpl = userServiceImpl;
     }
 
     @GetMapping("/users")
     public String findAll(Model model){
-        List<User> users = userService.findAll();
+        List<User> users = userServiceImpl.findAll();
         model.addAttribute("users", users);
         return "user-list";
     }
@@ -35,26 +35,26 @@ public class UserController {
 
     @PostMapping("/user-create")
     public String createUser(User user){
-        userService.saveUser(user);
+        userServiceImpl.saveUser(user);
         return "redirect:/users";
     }
 
     @GetMapping("user-delete/{id}")
     public String deleteUser(@PathVariable("id") Long id){
-        userService.deleteById(id);
+        userServiceImpl.deleteById(id);
         return "redirect:/users";
     }
 
     @GetMapping("/user-update/{id}")
     public String updateUserForm(@PathVariable("id") Long id, Model model){
-        User user = userService.findById(id);
+        User user = userServiceImpl.findById(id);
         model.addAttribute("user", user);
         return "user-update";
     }
 
     @PostMapping("/user-update")
     public String updateUser(User user){
-        userService.saveUser(user);
+        userServiceImpl.saveUser(user);
         return "redirect:/users";
     }
 }
--- src/main/java/net/proselyte/springbootdemo/model/User.java
+++ src/main/java/net/proselyte/springbootdemo/model/User.java
@@ -11,13 +11,18 @@ import javax.persistence.Table;
 
 @Data
 @Entity
-@Table(name = "users")
+@Table(name = "Users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "first_name")
-    private String firstName;
-    @Column(name = "last_name")
-    private String lastName;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "patronymic")
+    private String patronymic;
+    @Column(name = "number")
+    private String number;
+
 }
